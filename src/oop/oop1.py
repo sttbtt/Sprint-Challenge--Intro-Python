929ed5f1b515612cd680921696b308cf1bce5853
Scott Bennett <stt.btt@gmail.com>
2019-02-15 11:22:12 -0500

    oop1.py done

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,32 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    # Base Class
+    pass
+
+class FlightVehicle(Vehicle):
+    # Base Class Vehicle
+    pass
+
+class Starship(FlightVehicle):
+    # Base Class FlightVehicle
+    pass
+
+class GroundVehicle(Vehicle):
+    # Base Class Vehicle
+    pass
+
+class Airplane(FlightVehicle):
+    # Base Class FlightVehicle
+    pass
+
+class Car(GroundVehicle):
+    # Base Class GroundVehicle
+    pass
+
+class Motorcycle(GroundVehicle):
+    # Base Class GroundVehicle
+    pass
+
